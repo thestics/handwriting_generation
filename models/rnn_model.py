38b5641d5bff961b19e9e1d11424b93855657b35
Danil Kovalenko <danil.kovalenko@apostera.com>
2020-02-13 15:37:27 +0200

    mvp

--- models/rnn_model.py
+++ models/rnn_model.py
@@ -4,41 +4,30 @@
 
 
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense, Input, LSTM, Layer, TimeDistributed
 from tensorflow.keras.models import Model, Sequential
 
-from batch_generator import BatchGenerator
 
-
-def define_model(N):
+def define_model(N, batch_size, time_steps, vector_size):
     m = Sequential()
-    m.add(LSTM(units=N, activation='relu', return_sequences=True, input_shape=(None, 3)))
-    m.add(Dense(3, activation='linear'))
-    m.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    m.add(LSTM(units=N, activation='relu', return_sequences=True,
+               batch_input_shape=(batch_size, time_steps, vector_size)))
+    m.add(LSTM(units=N, activation='relu', return_sequences=True,
+               batch_input_shape=(batch_size, time_steps, vector_size)))
+    m.add(LSTM(units=N, activation='relu', return_sequences=True,
+               batch_input_shape=(batch_size, time_steps, vector_size)))
+    m.add(Dense(units=vector_size, activation='linear'))
+    m.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
     return m
-    # x = Input(shape=(None, 3))
-    # lstm1 = LSTM(units=N, activation='relu', return_sequences=True)(x)
-    # # lstm2 = LSTM(units=N, activation='relu', return_sequences=True)(lstm1)
-    # # lstm3 = LSTM(units=N, activation='relu', return_sequences=True)(lstm2)
-    # out = Dense(3, activation='linear')(lstm3)
-    #
-    # m = Model(inputs=x, outputs=out)
-    # m.compile(optimizer='adam', loss='categorical_crossentropy',
-    #           metrics=['accuracy'])
-    # return m
 
 
 if __name__ == '__main__':
-    N = 10
-    batch_size = 1053
-
-    # size = 10001
-    m = define_model(N)
+    N = 40
     X = np.load('../lines.npy', allow_pickle=True)
-    X_train = X[:, :-1, :]
-    Y_train = X[:, 1, :]
-    m.build()
-    m.summary()
-    m.fit(X_train, Y_train)
+    batch_size = 32
+    _, time_steps, vector_size = X.shape
+    m = define_model(N, 32, time_steps - 1, vector_size)
+    size = X.shape[0] - X.shape[0] % batch_size
+    X_train = X[:size, :-1, :]
+    Y_train = X[:size, 1:, :]
+    m.fit(X_train, Y_train, batch_size=batch_size, epochs=1)
